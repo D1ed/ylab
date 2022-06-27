713ec5ed868a4d6e93738f3546f2863d05ee189b
Lev Raspopov <lgraspopov@gmail.com>
2022-06-28 03:42:53 +0500

    Задание № 5
    обновлен способ просчёта на перемножение чисел списка primesL (раньше было перебор всех чисел до limit и сравнение их с primesL)

--- homework1/find_factorials.py
+++ homework1/find_factorials.py
@@ -1,33 +1,23 @@
-def Factor(n):
-    Ans = []
-    d = 2
-    while d * d <= n:
-        if n % d == 0:
-            Ans.append(d)
-            n //= d
-        else:
-            d += 1
-    if n > 1:
-        Ans.append(n)
-    return Ans
-
-
 def count_find_num(primesL, limit):
-    count = 0
-    end = []
-    end_i = 1
-    for n in reversed(range(limit+1)):
-        if list(set(Factor(n))) == primesL:
-            count += 1
-            end += [Factor(n)]
-
-    if len(end):
-        for _, n in enumerate(end[0]):
-            end_i *= n
-        return [count, end_i]
-    else:
+    if multiply_all_primesL(primesL) > limit:
         return []
+    res = list()
+    res.append(multiply_all_primesL(primesL))
+    for i in primesL:
+        for value in res:
+            value *= i
+            while value <= limit and value not in res:
+                res.append(value)
+                value *= i
+    end = max(res)
+    count = len(res)
+    return [count, end]
 
+def multiply_all_primesL(s):
+    ended_prime = 1
+    for i in s:
+        ended_prime *= i
+    return ended_prime
 
 primesL = [2, 3]
 limit = 200
@@ -44,6 +34,16 @@ limit = 500
 assert count_find_num(primesL, limit) == [12, 480]
 print(count_find_num(primesL, limit))
 
+primesL = [2,5,7]
+limit = 500
+assert count_find_num(primesL, limit) == [5, 490]
+print(count_find_num(primesL, limit))
+
+primesL = [3,5,7]
+limit = 500
+assert count_find_num(primesL, limit) == [2, 315]
+print(count_find_num(primesL, limit))
+
 primesL = [2, 3, 5]
 limit = 1000
 assert count_find_num(primesL, limit) == [19, 960]
@@ -52,4 +52,14 @@ print(count_find_num(primesL, limit))
 primesL = [2, 3, 47]
 limit = 200
 assert count_find_num(primesL, limit) == []
+print(count_find_num(primesL, limit))
+
+primesL = [2, 5, 23, 61]
+limit = 1063585
+assert count_find_num(primesL, limit) ==  [16, 897920]
+print(count_find_num(primesL, limit))
+
+primesL = [19, 29, 61]
+limit = 2571423
+assert count_find_num(primesL, limit) ==  [4, 2050271]
 print(count_find_num(primesL, limit))
