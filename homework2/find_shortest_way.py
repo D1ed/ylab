077fa0df6a2a5d81e34d5b440168ef64011553d8
Lev Raspopov <lgraspopov@gmail.com>
2022-07-05 19:16:47 +0500

    Задача № 1 (Переписан метод просчёта для добавления других городов)

--- homework2/find_shortest_way.py
+++ homework2/find_shortest_way.py
@@ -3,30 +3,53 @@ point_2 = (2, 5)
 point_3 = (5, 2)
 point_4 = (6, 6)
 point_5 = (8, 3)
+#point_6 = (20, 16)
 
 all_points = [point_1,point_2,point_3,point_4,point_5]
+#all_points = [point_1,point_2,point_3,point_4,point_5,point_6]
 
-towns = [[],[],[],[],[]]
-for c in range(5):
+from sys import maxsize
+from itertools import permutations
+
+towns = [[] for i in range(len(all_points))]
+
+
+for c in range(len(all_points)):
     for i in range(len(all_points)):
         towns[c].append(((all_points[c][0] - all_points[i][0]) ** 2 + (all_points[c][1] - all_points[i][1]) ** 2) ** 0.5)
+ 
+def tsp(towns):
+    vertex = []
+    store = []
+    store = []
+    for i in range(len(all_points)):
+        if i != 0:
+            vertex.append(i)
+    min_path = maxsize
+    next_permutation=permutations(vertex)
+    for i in next_permutation:
+        store.append(i)
+        current_pathweight = 0
+        k = 0
+        for j in i:
+            current_pathweight += towns[k][j]
+            k = j
+        current_pathweight += towns[k][0]
+        store.append(current_pathweight)
+        min_path = min(min_path, current_pathweight)
+    x = 0
+    res = ()
+    while x != 1:
+        for i in range(len(store)):
+            if store[i] == min_path:
+                res = store[i-1]
+                x = 1
+    end = str(all_points[0])
+    start_point = 0           
+    for i in res:
+        end += ' → ' + str(all_points[i])+ '[' + str(towns[start_point][i]) + ']'
+        start_point = i
+    end += ' → ' + str(all_points[0])+ '[' + str(towns[start_point][0]) + '] = ' + str(min_path)
+    return print(end)
 
-path =[]
-counter = 0
-minPath = 10000
-minCounter = 0
-i1 = 0
-
-for i2 in range(5):
-    for i3 in range(5):
-        for i4 in range(5):
-            for i5 in range(5):
-                if i1 != i2 and i1 != i3 and i1 != i4 and i1 != i5 and i2 != i3 and i2 != i4 and i2 != i5 and i3 != i4 and i3 != i5 and i4 != i5:
-                    path.append(str(all_points[i1]) + ' → ' + str(all_points[i2])+ '[' + str(towns[i1][i2]) + '] → ' + str(all_points[i3])+ '[' + str(towns[i2][i3]) + '] → ' + str(all_points[i4])+ '[' + str(towns[i3][i4]) + '] → ' + str(all_points[i5])+ '[' + str(towns[i4][i5]) + '] → ' + str(all_points[i1])+ '[' + str(towns[i5][i1])+ '] = ')
-                    if (towns[i1][i2] + towns[i2][i3] + towns[i3][i4] + towns[i4][i5] + towns[i5][i1]) < minPath:
-                        minPath = towns[i1][i2] + towns[i2][i3] + towns[i3][i4] + towns[i4][i5] + towns[i5][i1]
-                        minCounter = counter
-                    counter +=1
-
-result = path[minCounter]+str(minPath)
-print(result)
+tsp(towns)
